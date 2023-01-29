6c7ae36262cc33434dff7d13833e772c31fb7344
gianramirez <radicgian@gmail.com>
2023-01-28 21:42:12 -0500

    began work on user model. Mob

--- src/main/java/com/alumni/HealthChecker/models/User.java
+++ src/main/java/com/alumni/HealthChecker/models/User.java
@@ -1,4 +1,32 @@
 package com.alumni.HealthChecker.models;
 
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.GeneratedValue;
+
 public class User {
+    @Id
+    @GeneratedValue
+
+    private Long id;
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String profilePicture;
+    private String password;
+    private String country;
+    private String state;
+    private String county;
+    private String city;
+    private String address1;
+    private String address2;
+    private String zipCode;
+
+
+
+
+
 }
