1b1c01635b1a5f4098fb48612158cb52a0845c07
IamDevSteph <stephanie.ingraham3493@gmail.com>
2023-02-11 21:04:43 -0500

    Added more meat to both Diagnosis and Symptoms classes

--- src/main/java/com/alumni/HealthChecker/models/Diagnosis.java
+++ src/main/java/com/alumni/HealthChecker/models/Diagnosis.java
@@ -1,8 +1,12 @@
 package com.alumni.HealthChecker.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @Entity
 public class Diagnosis {
@@ -11,14 +15,18 @@ public class Diagnosis {
     @GeneratedValue
     public long id;
     public String name;
+    @OneToMany(mappedBy = "diagnosis")
+    @JsonIgnore
+    public Symptoms symptoms;
 
     public Diagnosis(){
 
     }
 
-    public Diagnosis(Long id, String name){
+    public Diagnosis(Long id, String name, Symptoms symptoms){
         this.id = id;
         this.name = name;
+        this.symptoms = symptoms;
     }
 
     public Long getId(){
@@ -27,5 +35,6 @@ public class Diagnosis {
     public String getName(){
         return name;
     }
+    public Symptoms getSymptoms(){ return symptoms; }
     //create diagnosis repository and diagnosis controller. Need to create One to Many for Diagnosis and Many to Many for Symptoms.
 }
--- src/main/java/com/alumni/HealthChecker/models/Symptoms.java
+++ src/main/java/com/alumni/HealthChecker/models/Symptoms.java
@@ -4,6 +4,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Symptoms {
@@ -13,15 +14,18 @@ public class Symptoms {
     public Long id;
     public String name;
     public String description;
+    @ManyToOne
+    public Diagnosis diagnosis;
 
     public Symptoms(){
 
     }
 
-    public Symptoms(Long id, String name, String description){
+    public Symptoms(Long id, String name, String description, Diagnosis diagnosis){
         this.id = id;
         this.name = name;
         this.description = description;
+        this.diagnosis = diagnosis;
     }
 
     public Long getId(){
