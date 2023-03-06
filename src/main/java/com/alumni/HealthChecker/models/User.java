c3e327fb110e8bd311c9e4a48dd2205b27076fb5
IamDevSteph <stephanie.ingraham3493@gmail.com>
2023-03-05 20:13:35 -0500

    mobbed with Gian Ramirez/Added parameters to User class

--- src/main/java/com/alumni/HealthChecker/models/User.java
+++ src/main/java/com/alumni/HealthChecker/models/User.java
@@ -1,4 +1,38 @@
 package com.alumni.HealthChecker.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="users")
 public class User {
+
+    //id (int/PRIMARY KEY), name(String/TEXT), age(int) ???
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String name;
+    public int age;
+
+    public User(){
+
+    }
+
+    public User(Long id, String name, int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
