40a1438c8f28aef32533a8bcee1ae98e1abba238
IamDevSteph <stephanie.ingraham3493@gmail.com>
2023-03-05 20:57:56 -0500

    Imported User class into Repo

--- src/main/java/com/alumni/HealthChecker/repositories/UserRepository.java
+++ src/main/java/com/alumni/HealthChecker/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package com.alumni.HealthChecker.repositories;
 
+import com.alumni.HealthChecker.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
